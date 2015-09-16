fada8ff913b91c965c5d207f21c077986759bb6a
Brian Crosby <bcrosby@LT-21069.local>
2015-09-16 11:28:12 -0400

    Added Photo data and styled output

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  photoController: Ember.inject.controller('photo')
 });
--- app/routes/photos.js
+++ app/routes/photos.js
@@ -1,7 +1,53 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  setupController(controller, gallery) {
-    controller.set('title', 'My Gallery');
+  model() {
+    return [
+      {
+        title: 'Photo 1',
+        description: 'This is the first',
+        location: 'Columbus, OH',
+        dateTaken: new Date('09-05-2015'),
+        aperture: 'f/2.8',
+        shutter: '1/50',
+        iso: 100
+      },
+      {
+        title: 'Photo 2',
+        description: 'This is the second',
+        location: 'Columbus, OH',
+        dateTaken: new Date('09-05-2015'),
+        aperture: 'f/16',
+        shutter: '1/250',
+        iso: 100
+      },
+      {
+        title: 'Photo 3',
+        description: 'This is the third',
+        location: 'Columbus, OH',
+        dateTaken: new Date('09-07-2015'),
+        aperture: 'f/2.8',
+        shutter: '1/500',
+        iso: 100
+      },
+      {
+        title: 'Photo 4',
+        description: 'This is the forth',
+        location: 'Columbus, OH',
+        dateTaken: new Date('09-08-2015'),
+        aperture: 'f/5.6',
+        shutter: '1/150',
+        iso: 100
+      },
+      {
+        title: 'Photo 5',
+        description: 'This is the fifth',
+        location: 'Columbus, OH',
+        dateTaken: new Date('09-15-2015'),
+        aperture: 'f/2.8',
+        shutter: '1/50',
+        iso: 100
+      }
+    ];
   }
 });
