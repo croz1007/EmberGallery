1e1a67f7f08b40e7cdaac52af25a4c440b1c7c71
Brian Crosby <bcrosby@LT-21069.local>
2015-09-17 09:47:32 -0400

    Added content security config details

--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,15 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self'",
+      'img-src': "'self' 'unsafe-eval' *",
+      'style-src': "'self' 'unsafe-eval' *",
+      'media-src': "'self'"
     }
   };
 
